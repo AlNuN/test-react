251ea19263d8dbd2a453aeb988d04b7634fe9f59
Matheus Alves Nunes <matheus.alnun@gmail.com>
2021-02-15 09:18:15 -0300

    Firing events

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App, { calcularNovoSaldo } from './App';
 
 describe('Componente principal', () => {
@@ -35,6 +35,22 @@ describe('Componente principal', () => {
       };
       const novoSaldo = calcularNovoSaldo(valores, saldo);
       expect(novoSaldo).toBe(-40);
+    });
+    it('que é um saque, a transação deve ser realizada', () => {
+      const {getByText, getByTestId, getByLabelText} = render(<App />);
+
+      const saldo = getByText('R$ 1000');
+      const transacao = getByLabelText('Saque');
+      const valor = getByTestId('valor');
+      const botaoTransacao = getByText('Realizar operação');
+
+      expect(saldo.textContent).toBe('R$ 1000');
+
+      fireEvent.click(transacao, {target: {value: 'saque'}});
+      fireEvent.change(valor, {target: {value: 10}})
+      fireEvent.click(botaoTransacao);
+
+      expect(saldo.textContent).toBe('R$ 990');
     });
   });
 });
